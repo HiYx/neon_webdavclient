7f5d6815f16ea8443036589272e72adb58cf857b
yanyx2legion <meeason@foxmail.com>
2021-10-20 15:26:55 +0800

    脚本

--- WebdavClient-master/run.sh
+++ WebdavClient-master/run.sh
@@ -5,5 +5,6 @@
 # Created Time: 2021年10月20日 星期三 13时30分23秒
 #########################################################################
 #!/bin/bash
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hegaozhi/shortcut_git/Git_repo/neon/neon_webdavclient/WebdavClient-master/include/
 ./build/webrun
+
